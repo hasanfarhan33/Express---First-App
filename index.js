4eb50147b9a76377b33cecb1ddbab0340f84a09e
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-22 21:52:15 +0300

    Learnt about path parameters

--- index.js
+++ index.js
@@ -4,16 +4,56 @@ const app = express();
 
 const port = 3000;
 
-app.use((req, res) => {
-  console.log("We got a new request");
-  console.dir(req);
-  //   res.send("Hello we got your request. This is a response.");
-  //   res.send({ color: "red" });
-  res.send("<h1>This is my webpage</h1>");
+// app.use((req, res) => {
+//   console.log("We got a new request");
+//   //   console.dir(req);
+//   //   res.send("Hello we got your request. This is a response.");
+//   //   res.send({ color: "red" });
+//   res.send("<h1>This is my webpage</h1>");
+// });
+
+// Routing
+app.get("/", (req, res) => {
+  res.send("This is the home page.");
+});
+
+// EXPRESS PATH PARAMETERS
+app.get("/r/:subreddit", (req, res) => {
+  console.log(req.params);
+  const { subreddit } = req.params;
+  //   res.send("This is a subreddit!");
+  res.send(`Browsing the ${subreddit} subreddit!`);
+});
+
+app.get("/r/:subreddit/:postId", (req, res) => {
+  console.log(req.params);
+  const { subreddit, postId } = req.params;
+  //   res.send("This is a subreddit!");
+  res.send(`Viewing post ID: ${postId} on the ${subreddit} subreddit!`);
+});
+
+// /cats => meow
+app.get("/cats", (req, res) => {
+  console.log("Cats request");
+  res.send("Meow");
+});
+
+// /dogs => woof
+app.get("/dogs", (req, res) => {
+  console.log("Dogs request");
+  res.send("Woof");
 });
 
 app.listen(port, () => {
   console.log("Listening on port 3000");
 });
 
-// Request and Response
+// Post requests
+app.post("/cats", (req, res) => {
+  res.send("Post request to /cats");
+});
+
+// Generic Response
+app.get("*", (req, res) => {
+  res.send("I don't know that path!");
+});
